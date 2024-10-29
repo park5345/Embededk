accc583336d489cb8b120fd4775c7333fe5972c2
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-10-29 17:14:20 +0900

    24 10 29

--- javabasic/src/javabasic/ExLoopStatement2.java
+++ javabasic/src/javabasic/ExLoopStatement2.java
@@ -66,32 +66,24 @@ public class ExLoopStatement2 {
 		// 별짓기 6
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				if ( i == j  ) { 
-					System.out.print("*");
-				}else if( i == 0 && j == 4) {
-					System.out.print("*");
-				}else if ( i == 1 && j == 3) {
-					System.out.print("*");
-				}else if ( i == 3 && j == 1) {
-					System.out.print("*");
-				}else if ( i == 4 && j == 0) {
-					System.out.print("*");
-				}else
-			System.out.print("  ");
-		}
+				if ( i == j  ) {System.out.print("*");}
+				else if( i == 0 && j == 4) {System.out.print("*");}
+				else if ( i == 1 && j == 3) {System.out.print("*");}
+				else if ( i == 3 && j == 1) {System.out.print("*");}
+				else if ( i == 4 && j == 0) {System.out.print("*");}
+				else System.out.print("  ");}
 			System.out.println();
 		}
 		System.out.println("                                             ");
 		// 별짓기 7
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				if (i == 1 && j == 2) {
-					System.out.print("  ");
-				}else if (i == 2 && j <= 3 && j >=1 && j !=2 ) {
-					System.out.print("  ");
-				}else if (i==3 && j ==2) {
-					System.out.print("  ");
-				}else
+				if (i == 1 && j == 2) {System.out.print("  ");}
+				else if (i == 2 && j <= 3 && j >=1 && j !=2 ) 
+				{System.out.print("  ");}
+				else if (i==3 && j ==2) 
+				{System.out.print("  ");}
+				else
 				System.out.print("*");
 			}
 			System.out.println();
